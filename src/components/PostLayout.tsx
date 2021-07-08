ceece9724a4ee57bc9600d8be38c13454a8fc501
Juliano Farias <juliano@frontendwizard.dev>
2021-07-08 02:24:25 +0200

    feature: allow multiple authors and reviewers on post

--- src/components/PostLayout.tsx
+++ src/components/PostLayout.tsx
@@ -18,7 +18,8 @@ type Props = {
   date: Date;
   slug: string;
   tags: string[];
-  author: string;
+  authors: string[];
+  reviewers: string[];
   description?: string;
   children: React.ReactNode;
 };
@@ -26,13 +27,14 @@ export default function PostLayout({
   title,
   date,
   slug,
-  author,
+  authors,
+  reviewers,
   tags,
   description = "",
   children,
 }: Props) {
-  const keywords = tags.map(it => getTag(it).name);
-  const authorName = getAuthor(author).name;
+  const keywords = tags.map((it) => getTag(it).name);
+  const authorName = getAuthor(authors[0]).name;
   return (
     <Layout>
       <BasicMeta
@@ -68,7 +70,9 @@ export default function PostLayout({
                 <Date date={date} />
               </div>
               <div>
-                <Author author={getAuthor(author)} />
+                {authors.map((author) => (
+                  <Author key={author} author={getAuthor(author)} />
+                ))}
               </div>
             </div>
           </header>
@@ -90,148 +94,148 @@ export default function PostLayout({
       </div>
       <style jsx>
         {`
-            .container {
-              display: block;
-              max-width: 36rem;
-              width: 100%;
-              margin: 0 auto;
-              padding: 0 1.5rem;
-              box-sizing: border-box;
-              z-index: 0;
-            }
-            .metadata div {
-              display: inline-block;
-              margin-right: 0.5rem;
-            }
-            article {
-              flex: 1 0 auto;
-            }
-            h1 {
-              margin: 0 0 0.5rem;
-              font-size: 2.25rem;
-            }
-            .tag-list {
-              list-style: none;
-              text-align: right;
-              margin: 1.75rem 0 0 0;
-              padding: 0;
-            }
-            .tag-list li {
-              display: inline-block;
-              margin-left: 0.5rem;
-            }
-            .social-list {
-              margin-top: 3rem;
-              text-align: center;
-            }
+          .container {
+            display: block;
+            max-width: 36rem;
+            width: 100%;
+            margin: 0 auto;
+            padding: 0 1.5rem;
+            box-sizing: border-box;
+            z-index: 0;
+          }
+          .metadata div {
+            display: inline-block;
+            margin-right: 0.5rem;
+          }
+          article {
+            flex: 1 0 auto;
+          }
+          h1 {
+            margin: 0 0 0.5rem;
+            font-size: 2.25rem;
+          }
+          .tag-list {
+            list-style: none;
+            text-align: right;
+            margin: 1.75rem 0 0 0;
+            padding: 0;
+          }
+          .tag-list li {
+            display: inline-block;
+            margin-left: 0.5rem;
+          }
+          .social-list {
+            margin-top: 3rem;
+            text-align: center;
+          }
 
-            @media (min-width: 769px) {
-              .container {
-                display: flex;
-                flex-direction: column;
-              }
+          @media (min-width: 769px) {
+            .container {
+              display: flex;
+              flex-direction: column;
             }
-          `}
+          }
+        `}
       </style>
       <style global jsx>
         {`
-            /* Syntax highlighting */
-            .token.comment,
-            .token.prolog,
-            .token.doctype,
-            .token.cdata,
-            .token.plain-text {
-              color: #6a737d;
-            }
+          /* Syntax highlighting */
+          .token.comment,
+          .token.prolog,
+          .token.doctype,
+          .token.cdata,
+          .token.plain-text {
+            color: #6a737d;
+          }
 
-            .token.atrule,
-            .token.attr-value,
-            .token.keyword,
-            .token.operator {
-              color: #d73a49;
-            }
+          .token.atrule,
+          .token.attr-value,
+          .token.keyword,
+          .token.operator {
+            color: #d73a49;
+          }
 
-            .token.property,
-            .token.tag,
-            .token.boolean,
-            .token.number,
-            .token.constant,
-            .token.symbol,
-            .token.deleted {
-              color: #22863a;
-            }
+          .token.property,
+          .token.tag,
+          .token.boolean,
+          .token.number,
+          .token.constant,
+          .token.symbol,
+          .token.deleted {
+            color: #22863a;
+          }
 
-            .token.selector,
-            .token.attr-name,
-            .token.string,
-            .token.char,
-            .token.builtin,
-            .token.inserted {
-              color: #032f62;
-            }
+          .token.selector,
+          .token.attr-name,
+          .token.string,
+          .token.char,
+          .token.builtin,
+          .token.inserted {
+            color: #032f62;
+          }
 
-            .token.function,
-            .token.class-name {
-              color: #6f42c1;
-            }
+          .token.function,
+          .token.class-name {
+            color: #6f42c1;
+          }
 
-            /* language-specific */
+          /* language-specific */
 
-            /* JSX */
-            .language-jsx .token.punctuation,
-            .language-jsx .token.tag .token.punctuation,
-            .language-jsx .token.tag .token.script,
-            .language-jsx .token.plain-text {
-              color: #24292e;
-            }
+          /* JSX */
+          .language-jsx .token.punctuation,
+          .language-jsx .token.tag .token.punctuation,
+          .language-jsx .token.tag .token.script,
+          .language-jsx .token.plain-text {
+            color: #24292e;
+          }
 
-            .language-jsx .token.tag .token.attr-name {
-              color: #6f42c1;
-            }
+          .language-jsx .token.tag .token.attr-name {
+            color: #6f42c1;
+          }
 
-            .language-jsx .token.tag .token.class-name {
-              color: #005cc5;
-            }
+          .language-jsx .token.tag .token.class-name {
+            color: #005cc5;
+          }
 
-            .language-jsx .token.tag .token.script-punctuation,
-            .language-jsx .token.attr-value .token.punctuation:first-child {
-              color: #d73a49;
-            }
+          .language-jsx .token.tag .token.script-punctuation,
+          .language-jsx .token.attr-value .token.punctuation:first-child {
+            color: #d73a49;
+          }
 
-            .language-jsx .token.attr-value {
-              color: #032f62;
-            }
+          .language-jsx .token.attr-value {
+            color: #032f62;
+          }
 
-            .language-jsx span[class="comment"] {
-              color: pink;
-            }
+          .language-jsx span[class="comment"] {
+            color: pink;
+          }
 
-            /* HTML */
-            .language-html .token.tag .token.punctuation {
-              color: #24292e;
-            }
+          /* HTML */
+          .language-html .token.tag .token.punctuation {
+            color: #24292e;
+          }
 
-            .language-html .token.tag .token.attr-name {
-              color: #6f42c1;
-            }
+          .language-html .token.tag .token.attr-name {
+            color: #6f42c1;
+          }
 
-            .language-html .token.tag .token.attr-value,
-            .language-html
-              .token.tag
-              .token.attr-value
-              .token.punctuation:not(:first-child) {
-              color: #032f62;
-            }
+          .language-html .token.tag .token.attr-value,
+          .language-html
+            .token.tag
+            .token.attr-value
+            .token.punctuation:not(:first-child) {
+            color: #032f62;
+          }
 
-            /* CSS */
-            .language-css .token.selector {
-              color: #6f42c1;
-            }
+          /* CSS */
+          .language-css .token.selector {
+            color: #6f42c1;
+          }
 
-            .language-css .token.property {
-              color: #005cc5;
-            }
-          `}
+          .language-css .token.property {
+            color: #005cc5;
+          }
+        `}
       </style>
     </Layout>
   );
--- src/pages/posts/[post].tsx
+++ src/pages/posts/[post].tsx
@@ -6,7 +6,7 @@ import matter from "gray-matter";
 import { fetchPostContent } from "../../lib/posts";
 import fs from "fs";
 import yaml from "js-yaml";
-import { parseISO } from 'date-fns';
+import { parseISO } from "date-fns";
 import PostLayout from "../../components/PostLayout";
 
 import InstagramEmbed from "react-instagram-embed";
@@ -18,15 +18,16 @@ export type Props = {
   dateString: string;
   slug: string;
   tags: string[];
-  author: string;
+  authors: string[];
+  reviewers: string[];
   description?: string;
   source: MdxRemote.Source;
 };
 
 const components = { InstagramEmbed, YouTube, TwitterTweetEmbed };
-const slugToPostContent = (postContents => {
-  let hash = {}
-  postContents.forEach(it => hash[it.slug] = it)
+const slugToPostContent = ((postContents) => {
+  let hash = {};
+  postContents.forEach((it) => (hash[it.slug] = it));
   return hash;
 })(fetchPostContent());
 
@@ -35,27 +36,29 @@ export default function Post({
   dateString,
   slug,
   tags,
-  author,
+  authors,
+  reviewers,
   description = "",
   source,
 }: Props) {
-  const content = hydrate(source, { components })
+  const content = hydrate(source, { components });
   return (
     <PostLayout
       title={title}
       date={parseISO(dateString)}
       slug={slug}
       tags={tags}
-      author={author}
+      authors={authors}
+      reviewers={reviewers}
       description={description}
     >
       {content}
     </PostLayout>
-  )
+  );
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  const paths = fetchPostContent().map(it => "/posts/" + it.slug);
+  const paths = fetchPostContent().map((it) => "/posts/" + it.slug);
   return {
     paths,
     fallback: false,
@@ -66,7 +69,9 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   const slug = params.post as string;
   const source = fs.readFileSync(slugToPostContent[slug].fullPath, "utf8");
   const { content, data } = matter(source, {
-    engines: { yaml: (s) => yaml.load(s, { schema: yaml.JSON_SCHEMA }) as object }
+    engines: {
+      yaml: (s) => yaml.load(s, { schema: yaml.JSON_SCHEMA }) as object,
+    },
   });
   const mdxSource = await renderToString(content, { components, scope: data });
   return {
@@ -76,9 +81,9 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
       slug: data.slug,
       description: "",
       tags: data.tags,
-      author: data.author,
-      source: mdxSource
+      authors: data.authors,
+      reviewers: data.reviewers,
+      source: mdxSource,
     },
   };
 };
-
