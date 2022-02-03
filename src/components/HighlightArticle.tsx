b2995a91699de400180e37eb8623a3510ba5d47b
Juliano Farias <juliano@frontendwizard.dev>
2022-02-03 15:31:13 +0100

    fix: make HighlightArticle image full width

--- src/components/HighlightArticle.tsx
+++ src/components/HighlightArticle.tsx
@@ -62,12 +62,10 @@ const DesktopArticle = ({ postContent }: HighlightArticleProps) => {
     >
       <Image
         src="https://images.unsplash.com/photo-1515378791036-0648a3ef77b2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
-        layout="fill"
         objectFit="cover"
         alt="article's image"
         position="absolute"
-        top={0}
-        left={0}
+        w="full"
       />
       <Container maxW={'container.xl'}>
         <Stack
